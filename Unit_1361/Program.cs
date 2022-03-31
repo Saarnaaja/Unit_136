ac10778a6c5b1d5e425fbb4ed87f3d8abdb1b6d1
Saarnaaja <Norilsk.Devil@gmail.com>
2022-03-31 12:12:33 +0700

    Добавьте файлы проекта.

---
+++ Unit_1361/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Unit_1361
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = InputFileName();
+            var iterationCount = InputIterationCount();
+            Console.WriteLine("Время на вставку в List<T>");
+            EstimateList(WriteToList, iterationCount, fileName);
+            Console.WriteLine();
+            Console.WriteLine("Время на вставку в LinkedList<T>");
+            EstimateList(WriteToLinkedList, iterationCount, fileName);
+            Console.ReadLine();
+        }
+
+        static string InputFileName()
+        {
+            Console.WriteLine("Введите имя файла");
+            while (true)
+            {
+                var fileName = Console.ReadLine();
+                if (File.Exists(fileName)) return fileName;
+                else Console.WriteLine("Файл не найден");
+            }
+        }
+
+        static int InputIterationCount()
+        {
+            Console.WriteLine("Введите количество итераций");
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int result))
+                {
+                    if (result > 0) return result;
+                    else Console.WriteLine("Значение должно быть > 0");
+                }
+                else Console.WriteLine("Введено не число");
+            }
+        }
+
+        static void EstimateList(Action<string> action, int iterationCount, string fileName)
+        {
+            var max = 0L;
+            var min = long.MaxValue;
+            var avg = 0L;
+            var stopWath = new Stopwatch();
+            for (int i = 0; i < iterationCount; i++)
+            {
+                Thread.Sleep(500);
+                stopWath.Restart();
+                action.Invoke(fileName);
+                stopWath.Stop();
+                if (min > stopWath.ElapsedMilliseconds) min = stopWath.ElapsedMilliseconds;
+                if (max < stopWath.ElapsedMilliseconds) max = stopWath.ElapsedMilliseconds;
+                avg += stopWath.ElapsedMilliseconds;
+                Console.WriteLine($"Итерация {i + 1}/{iterationCount}: Затрачено времени: {stopWath.ElapsedMilliseconds}мс");
+            }
+            avg /= iterationCount;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Минимально затраченное время на вставку: {min}мс");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Максимально затраченное время на вставку: {max}мс");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine($"Среднее арифметическое затраченное время на вставку: {avg}мс");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        static void WriteToList(string fileName)
+        {
+            var list = new List<string>();
+            foreach (var line in File.ReadLines(fileName))
+            {
+                list.Add(line);
+            }
+        }
+
+        static void WriteToLinkedList(string fileName)
+        {
+            var linkedList = new LinkedList<string>();
+            foreach (var line in File.ReadLines(fileName))
+            {
+                linkedList.AddLast(line);
+            }
+        }
+    }
+}
---
+++ Unit_1361/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Unit_1361")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Unit_1361")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ced69eaf-cf86-4b24-8093-dd5ebbce5fa5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
